cc1946c1d14a281b1f6b4cc5668f9e74a2861208
dahee <daheewa128@gmail.com>
2021-08-12 23:52:43 +0900

    70. Climbing Stairs 322. Coin Change

---
+++ src/Dynamic_Programming/Climbing_Stairs.java
@@ -0,0 +1,31 @@
+package Dynamic_Programming;
+
+//70. Climbing Stairs
+public class Climbing_Stairs {
+    public static void main(String[] args) {
+        int n=3;
+        System.out.println(climbStairs2(n));
+    }
+    //too much time
+    public static int climbStairs2(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        return climbStairs(n-1)+climbStairs(n-2);
+    }
+
+    public static int climbStairs(int n) {
+
+        int[] dp=new int[n+1];
+        for(int i=0;i<=n;i++){
+            if(i<=2){
+                dp[i]=i;
+            }else{
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+
+}
---
+++ src/Dynamic_Programming/Coin_Change.java
@@ -0,0 +1,27 @@
+package Dynamic_Programming;
+
+import java.util.Arrays;
+//322. Coin Change
+public class Coin_Change {
+
+    public static void main(String[] args) {
+        int[] coins={1,2147483647};
+        int amount=2;
+        System.out.println(coinChange(coins,amount));
+    }
+    public static int coinChange(int[] coins, int amount) {
+        if(amount==0) return 0;
+
+        int[] arr=new int[amount+1];
+        Arrays.fill(arr,Integer.MAX_VALUE);
+        arr[0]=0;
+        for(int i=0;i<=amount;i++){
+            if(arr[i]==Integer.MAX_VALUE) continue;
+            for(int coin:coins){
+                if(coin>amount-i) continue;
+                arr[coin+i]=Math.min(arr[i]+1,arr[i+coin]);
+            }
+        }
+        return arr[amount]==Integer.MAX_VALUE?-1:arr[amount];
+    }
+}
